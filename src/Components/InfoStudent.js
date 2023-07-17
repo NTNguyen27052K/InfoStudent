e7c480bc6e0461a32602a462154705fe0d094d28
NTNguyen27052K <ntnguyen275@gmail.com>
2023-07-18 00:45:51 +0700

    Nineth

--- src/Components/InfoStudent.js
+++ src/Components/InfoStudent.js
@@ -10,7 +10,7 @@ export default class InfoStudent extends Component {
           style={{ backgroundColor: "rgb(49 55 61)", borderRadius: "10px" }}
           className="fs-1 p-3 text-white text-center mt-3"
         >
-          Thông tin sinh viên bài em chưa xong đừng chấm nha!
+          Thông tin sinh viên
         </h1>
         <FormInfo />
         {/* <TableListStudent /> */}
--- src/Components/test.js
+++
@@ -1,7 +0,0 @@
-import React, { useRef } from "react";
-
-const test = () => {
-  return <div>test</div>;
-};
-
-export default test;
